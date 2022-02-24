a6eb76206e71bffa1f7d245815ffeba39f64151c
Sagar Pachare <spachare91@gmail.com>
2022-02-24 08:44:42 +0530

    deploying project

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv')
 
 
 dotenv.config({path:"./config.env"});
-const port= 5000 || process.env.PORT;
+const port= process.env.PORT;
 try {
   const conn= mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
